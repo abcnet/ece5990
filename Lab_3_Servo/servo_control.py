ad31ad1327ddadcd02374e25c32362a8cbe4ef83
Zhiyun Ren <zr54@cornell.edu>
2016-03-16 18:14:31 -0400

    added lab 3 two_wheel and servo_control

--- Lab_3_Servo/servo_control.py
+++ Lab_3_Servo/servo_control.py
@@ -5,10 +5,15 @@ import sys
 import os
 import time
 
-
+GPIO.cleanup()
+dc_max = 8.5
+dc_min = 6.5
 dc=7.5
+dc_var = 7.5
 CHANNEL = 19
-freq =46
+freq=46.51
+inc = 0.1
+
 
 GPIO.setmode(GPIO.BCM) #Setforbroadcomnumberingnotboardnumbering
 
@@ -22,8 +27,11 @@ GPIO.setup(CHANNEL, GPIO.OUT)
 
 # To create a PWM instance:
 p = GPIO.PWM(CHANNEL, freq)
-# To start PWM:
-p.start(dc) # where dc is the duty cycle (0.0 <= dc <= 100.0)
+# Initialize servo to not moving
+p.start(dc)
+
+
+
 # To change the frequency:
 # p.ChangeFrequency(freq) # where freq is the new frequency in Hz
 # To change the duty cycle:
@@ -48,10 +56,32 @@ def GPIO23_callback(channel):
 
 
 #interrupt detection
-GPIO.add_event_detect(17, GPIO.FALLING, callback=GPIO17_callback, bouncetime=300)
+#GPIO.add_event_detect(17, GPIO.FALLING, callback=GPIO17_callback, bouncetime=300)
 GPIO.add_event_detect(22, GPIO.FALLING, callback=GPIO22_callback, bouncetime=300)
 GPIO.add_event_detect(23, GPIO.FALLING, callback=GPIO23_callback, bouncetime=300)
 
-
+time.sleep(5)
 while True:
-	pass
+	for x in range(1,11):
+		time.sleep(1)
+		dc_var += inc
+		p.ChangeDutyCycle(dc_var)
+		print x
+		ts = time.time()
+		p.start(dc_var)
+		while (time.time() - ts <= 3.0):
+			pass
+		p.stop()
+	dc_var = dc
+	print "Done counter-clockwise"
+	for x in range(1,10):
+		time.sleep(1)
+		dc_var -= inc
+		p.ChangeDutyCycle(dc_var)
+		ts = time.time()	
+		p.start(dc_var)
+		while (time.time() - ts <= 3.0):
+			pass
+		p.stop()
+	print "Done clockwise"
+	break
--- Lab_3_Servo/two_wheel.py
+++ Lab_3_Servo/two_wheel.py
@@ -6,9 +6,10 @@ import os
 import time
 
 
-dc=7.5
-CHANNEL = 19
-freq =46
+
+CHANNEL1 = 19
+CHANNEL2 = 16
+freq = 46.51
 
 GPIO.setmode(GPIO.BCM) #Setforbroadcomnumberingnotboardnumbering
 
@@ -16,35 +17,57 @@ GPIO.setmode(GPIO.BCM) #Setforbroadcomnumberingnotboardnumbering
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(CHANNEL, GPIO.OUT)
+GPIO.setup(CHANNEL1, GPIO.OUT)
 
 
 
 # To create a PWM instance:
-p = GPIO.PWM(CHANNEL, freq)
+p1 = GPIO.PWM(CHANNEL1, freq)
+p2 = GPIO.PWM(CHANNEL2, freq)
 # To start PWM:
-p.start(dc) # where dc is the duty cycle (0.0 <= dc <= 100.0)
+# p.start(dc) # where dc is the duty cycle (0.0 <= dc <= 100.0)
 # To change the frequency:
 # p.ChangeFrequency(freq) # where freq is the new frequency in Hz
 # To change the duty cycle:
 # p.ChangeDutyCycle(dc) # where 0.0 <= dc <= 100.0
 # To stop PWM:
 
-
+def drive_servo(servo_number, direction):
+	if servo_number==1:
+		p=p1
+	elif servo_number==2:
+		p=p2
+	else:
+		print "error"
+		return
+	if direction==0:
+		p.stop()
+	elif direction<0:
+		p.start()
+		p.ChangeFrequency(1000.0/21.7)
+		p.ChangeDutyCycle(1.7/21.7)
+	elif direction>0:
+		p.start()
+		p.ChangeFrequency(1000.0/21.3)
+		p.ChangeDutyCycle(1.3/21.3)
+	else:
+		print "error"
 
 
 #define interrupt handlers
 def GPIO17_callback(channel):
-	p.stop()
+	p1.stop()
+	p2.stop()
 	GPIO.cleanup()
-	sys.exit("Quitting Bounce Program")
+	sys.exit("Quitting Program")
 
 def GPIO22_callback(channel):
-	p.stop()
+	p1.stop()
+	p2.stop()
 
-def GPIO23_callback(channel):
-	p.start(dc)
-	p.ChangeFrequency(freq)
+# def GPIO23_callback(channel):
+# 	p.start(dc)
+# 	p.ChangeFrequency(freq)
 
 
 #interrupt detection
@@ -54,4 +77,6 @@ GPIO.add_event_detect(23, GPIO.FALLING, callback=GPIO23_callback, bouncetime=300
 
 
 while True:
-	pass
+	servo_number = raw_input("Chooese server (1 or 2): ")
+	direction = raw_input("Chooese direction (-1, 0 or 1): ")
+	drive_servo(servo_number, direction)
