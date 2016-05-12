f1c33eb0e5dfe30c1cfaba571a1d14bcbec75bef
Zhiyun Ren <zr54@cornell.edu>
2016-05-11 21:28:27 -0400

    modified test_camera

--- final_proj/test_camera.py
+++ final_proj/test_camera.py
@@ -2,29 +2,33 @@ import socket
 import time
 import picamera
 
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 24
 
-initial_socket = socket.socket()
-TCP_HOST = ''
-TCP_PORT = 8000
-initial_socket.bind((TCP_HOST, TCP_PORT))
-initial_socket.listen(5)
-print 'Socket now listening'
 
-#Accept a single connection and make a file-like object out of it
-accepted_conn = initial_socket.accept()
-connection = accepted_conn[0].makefile('wb')
-client_addr = accepted_conn[1][0]
 
-try:
-    camera.start_recording(connection, format='h264')
-    camera.wait_recording(60)
-    camera.stop_recording()
-finally:
-    connection.close()
-    initial_socket.close()
-    #server_socket.close()
-    #time.sleep(30);
-    camera.close()
+def camera_stream():
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 24
+
+    initial_socket = socket.socket()
+    TCP_HOST = ''
+    TCP_PORT = 8
+    initial_socket.bind((TCP_HOST, TCP_PORT))
+    initial_socket.listen(5)
+    print 'Socket now listening'
+
+    #Accept a single connection and make a file-like object out of it
+    accepted_conn = initial_socket.accept()
+    connection = accepted_conn[0].makefile('wb')
+    client_addr = accepted_conn[1][0]
+    
+    try:
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(60)
+        camera.stop_recording()
+    finally:
+        connection.close()
+        initial_socket.close()
+        #server_socket.close()
+        #time.sleep(30);
+        camera.close()
