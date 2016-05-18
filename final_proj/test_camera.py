d79c2612a9a0701c8d014a04f662f0c7cc2eb1e8
Zhiyun Ren <zr54@cornell.edu>
2016-05-17 22:15:47 -0400

    test_camer.py modified

--- final_proj/test_camera.py
+++ final_proj/test_camera.py
@@ -1,21 +1,29 @@
 import socket
 import time
 import picamera
+import sys
+import errno
 
 #Define the streaming method
-def camera_stream(passed_socket):    
+def camera_stream():    
     while 1:
+        #Create a TCP socket on the Raspberry Pi
+        tcp_sock = socket.socket()
+        tcp_ip = ''
+        tcp_port = 8767
+        tcp_sock.bind((tcp_ip, tcp_port))
+
         #listen for a new connection
-        passed_socket.listen(5)
+        tcp_sock.listen(5)
         print 'Server listening for connection'    
 
         #Accept a new connection
-        conn = passed_socket.accept()
+        conn = tcp_sock.accept()
         print 'Accepted Connection'
     
         #Initialize Camera
         camera = picamera.PiCamera()
-        camera.resolution = (360,240)
+        camera.resolution = (1920,1080)
         camera.framerate = 24        
         camera.vflip = True
         camera.hflip = True
@@ -31,16 +39,14 @@ def camera_stream(passed_socket):
                 camera.stop_recording()
                 #print 'Wrote data to connection'
     
-        finally:
-            connection.close()
-            camera.close()    
+        except Exception:
+            #conn.close()
+            #tcp_sock.close()
+            camera.close()
+               
+        time.sleep(0.1)
 
-        sleep(0.1)
+camera_stream()
 
-#Create a TCP socket on the Raspberry Pi
-tcp_sock = socket.socket()
-tcp_ip = ''
-tcp_port = 8767
-tcp_sock.bind((tcp_ip, tcp_port))
-
-camera_stream(tcp_sock)
+#if __name__ == '__main__'
+#   camera_stream()
