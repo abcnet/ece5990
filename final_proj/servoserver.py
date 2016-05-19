c1683a27e691806c75c1b04dc6325a4bf4a0c676
Zhiyun Ren <zr54@cornell.edu>
2016-05-19 15:03:41 -0400

    wifi thread

--- final_proj/servoserver.py
+++ final_proj/servoserver.py
@@ -58,50 +58,13 @@ if ON_RPI:
     GPIO.add_event_detect(17, GPIO.FALLING, callback=GPIO17_callback, bouncetime=300)
     p1 = GPIO.PWM(CHANNEL1, freq)
     p2 = GPIO.PWM(CHANNEL2, freq)
-# pulses=[no,no]
-# directions = [0, 0]
-# d=['STOP','STOP']
-# hist1=['STOP', 'STOP', 'STOP']
-# hist2=['STOP', 'STOP', 'STOP']
-# def drive_servo(servo_number, direction):
-#     if servo_number==1:
-#         p=p1
-#     elif servo_number==2:
-#         p=p2
-#     else:
-#         print "error"
-#         return
-#     if direction==0:
-#         p.stop()
-#     elif direction<0:
-#         p.start(ccw/(20+ccw)*100.0)
-#         p.ChangeFrequency(1000.0/(20+ccw))
-#         # p.ChangeDutyCycle(1.7/21.7)
-#     elif direction>0:
-#         p.start(cw/(20+cw)*100.0)
-#         p.ChangeFrequency(1000.0/(20+cw))
-#         # p.ChangeDutyCycle(1.3/21.3)
-#     else:
-#         print "error"
 
-# def drive(l,r,t):
-#     if kill:
-#         sys.exit("Quitting Program")
-#     drive_servo(1, l)
-#     drive_servo(2, r)
-#     while t>0:
-#         if kill:
-#             sys.exit("Quitting Program")
-#         time.sleep(1)
-#         if not panic:
-#             t -= 1
-#     drive_servo(1, 0)
-#     drive_servo(2, 0)
 
 
 
 host = "0.0.0.0"
 port = 8765
+port2 = 8766
 
 DEBUG = True
 
@@ -116,6 +79,7 @@ from shutil import copyfile
 from test1 import postIP
 
 nthreads = 32
+nthreads = 2
 # backupGroup = 32
 IDLE = 0
 AFTER_HELO = 1
@@ -130,6 +94,18 @@ DEBUG = True
 left = 0
 right = 0
 timeleft = 0
+iPhoneConnected = False
+
+def iPhoneConnect():
+    global iPhoneConnected
+    if DEBUG:
+        print "-----------iPhone connected---------"
+    iPhoneConnected = True
+def iPhoneDisconnect():
+    global iPhoneConnected
+    if DEBUG:
+        print '------------iPhone DISCONNECTED-------'
+    iPhoneConnected = False
 
 def writeCommand(command):
     print 'writing command'
@@ -184,40 +160,13 @@ def executeCommand():
             time.sleep(0.1)
             timeleft -= 0.1
         
+def door(command):
+    try:
+        if command[0] == '1':
+            pass
 
-# def checkCommand(l):
-#     # print l
-#     if len(l)<1:
-#         return ILLEGAL, 0
-#     first = l[0].upper()
-#     if first == 'HELO':
-#         return HELO, 0
-#     if first == 'DATA':
-#         return DATA, 0
-#     if len(l)<2:
-#         return ILLEGAL, 0
-#     second = l[1].upper()
-#     if first == 'MAIL':
-#         if second == 'FROM:':
-#             return MAIL_FROM, 0
-#         if len(l)>=3 and l[2]==':':
-#             return MAIL_FROM, 1
-#         if second[:5] == 'FROM:' and len(second) > 5:
-#             tmp = l[1][5:]
-#             l[1:2] = ['FROM:', tmp]
-#             return MAIL_FROM, 0
-#     if first == 'RCPT':
-#         if second == 'TO:':
-#             return RCPT_TO, 0
-#         if len(l)>=3 and l[2]==':':
-#             return RCPT_TO, 1
-#         if second[:3] == 'TO:' and len(second) > 3:
-#             tmp = l[1][3:]
-#             l[1:2] = ['TO:', tmp]
-#             return RCPT_TO, 0
-#     return ILLEGAL, 0
-
-# f = open('mailbox', 'w')
+    except Exception as e:
+        print e
 
 class ConnectionHandler:
     """Handles a single client request"""
@@ -233,6 +182,7 @@ class ConnectionHandler:
         # global f
         # state = IDLE
         # self.socket.send("From RPi server: connected\r\n")
+        iPhoneConnect()
         self.socket.settimeout(TIMEOUT)
         starttime = datetime.now()
         stringbuffer = ''
@@ -294,12 +244,96 @@ class ConnectionHandler:
             except socket.timeout:
                 # self.socket.send('From RPi server Error: timeout exceeded\r\n')
                 self.socket.close()
+                iPhoneDisconnect()
+                return
+            except socket.error as e:
+                print e
+                self.socket.close()
+                iPhoneDisconnect()
+                return
+
+class ConnectionHandler2:
+    """Handles a single client request"""
+    count = 0
+    lock = Lock()
+    locked = False
+    cond = Condition(lock)
+
+    def __init__(self, socket):
+        self.socket = socket
+
+    def handle2(self):
+        # global f
+        # state = IDLE
+        # self.socket.send("From RPi server: connected\r\n")
+        self.socket.settimeout(TIMEOUT)
+        starttime = datetime.now()
+        stringbuffer = ''
+        clientname = ''
+        sender = ''
+        recipients = []
+        data = []
+        while True:
+            try:
+                while stringbuffer.find('\r\n')==-1:
+                    # print 'cannot find \\r\\n'
+                    # collect TCP stream until <CR><LF> is reached
+                    # print stringbuffer
+                    stringbuffer += self.socket.recv(16)
+                    # print 'timenow'
+                    timenow = datetime.now()
+                    # calculate new timeout time
+                    elapsed = (timenow - starttime).total_seconds()
+                    # print 'elpased'
+                    if elapsed>=TIMEOUT:
+                        # self.socket.send('From RPi server Error: timeout exceeded\r\n')
+                        self.socket.close()
+                        return
+                    else:
+                        self.socket.settimeout(TIMEOUT - elapsed)
+                    # print 'end of inner while'
+                if DEBUG:
+                    print stringbuffer
+                index = stringbuffer.find('\r\n')
+                commandstring = stringbuffer[:index]
+                if DEBUG:
+                    print 'Wifi Thread: ', commandstring
+                command = commandstring.split()
+                stringbuffer = stringbuffer[index+2:]
+
+
+
+                
+
+                # success = 
+                print 'Wifi Thread: about to send received'
+                self.socket.send("Received %s\r\n" % commandstring)
+                print 'Wifi Thread: received sent'
+                self.socket.settimeout(TIMEOUT)
+                # if success:
+                    
+                #     self.socket.send("Success %s\r\n" % commandstring)
+                #     if DEBUG:
+                #         print 'Sent "Success %s\\r\\n" to iPhone' % commandstring
+                #     self.socket.settimeout(TIMEOUT)
+                # else:
+                    
+                #     self.socket.send("Failed %s\r\n" % commandstring)
+                #     if DEBUG:
+                #         print 'Sent "Failed %s\\r\\n" to iPhone' % commandstring
+                #     self.socket.settimeout(TIMEOUT)
+                # self.socket.close()
+                # return
+               
+
+            except socket.timeout:
+                # self.socket.send('From RPi server Error: timeout exceeded\r\n')
+                self.socket.close()
                 return
             except socket.error as e:
                 print e
                 self.socket.close()
                 return
-
 
 def serverloop():
     """The main server loop"""
@@ -313,6 +347,16 @@ def serverloop():
     # start listening with a backlog of 5 connections
     serversocket.listen(5)
 
+
+    serversocket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # mark the socket so we can rebind quickly to this port number
+    # after the socket is closed
+    serversocket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # bind the socket to the local loopback IP address and special port
+    serversocket2.bind((host, port2))
+    # start listening with a backlog of 5 connections
+    serversocket2.listen(5)
+
     def serverloop_singlethread():
 
         while True:
@@ -321,6 +365,14 @@ def serverloop():
             ct = ConnectionHandler(clientsocket)
             ct.handle()
 
+    def serverloop_singlethread2():
+
+        while True:
+            # accept a connection
+            (clientsocket2, address2) = serversocket2.accept()
+            ct2 = ConnectionHandler2(clientsocket2)
+            ct2.handle2()
+
     for i in range(nthreads):
         # print('Running thread %d' % i)
         thread = Thread(target=serverloop_singlethread)
@@ -328,6 +380,9 @@ def serverloop():
 
     servoThread = Thread(target=executeCommand)
     servoThread.start()
+
+    wifiThread = Thread(target=serverloop_singlethread2)
+    wifiThread.start()
 
 if __name__ == '__main__':
     if DEBUG:
